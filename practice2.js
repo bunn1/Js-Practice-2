1ed75b82d6e00c8d4b9b0d181c03aaf774ae452a
bunn1 <nicjohbun@gmail.com>
2022-06-29 23:19:25 +0200

    get URL Js

---
+++ practice2.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to get the website URL (loading page)
+
+document.getElementById("demo").innerHTML = 
+"The full URL of this page is:<br>" + window.location.href;
+
+// Write a JavaScript program to print the contents of the current window
+
+
