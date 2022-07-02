ebfe5c8522658fbdac8195550cb27cdac492f547
bunn1 <nicjohbun@gmail.com>
2022-07-02 09:47:01 +0200

    Js - function reverse and prompt chosen Number

--- practice2.js
+++ practice2.js
@@ -1,10 +1,35 @@
 //Write a JavaScript program to get the website URL (loading page)
 
-document.getElementById("demo").innerHTML = 
-"The full URL of this page is:<br>" + window.location.href;
+document.getElementById("demo").innerHTML =
+    "The full URL of this page is:<br>" + window.location.href;
 
 // Write a JavaScript program to print the contents of the current window
 
-function print_current_page () {
-    window.print();
+// function print_current_page() {
+//     window.print();
+// }
+
+// Write a JavaScript function that reverse a number
+
+
+const num = 12345;
+
+const reverse = (num) => parseInt(String(num)
+.split(' ')
+.reverse()
+.join(' '), 10);
+
+console.log(reverse(num))
+
+// Random number between 1 - 10
+
+let theRandomNumber = Math.floor(Math.random() * 10) + 1
+console.log(theRandomNumber)
+let chosenNumber = prompt("Enter a number")
+
+if (theRandomNumber === chosenNumber) {
+       console.log("Good Work")
+}else {
+     console.log("Not Matched")
 }
+// console.log(theRandomNumber)
